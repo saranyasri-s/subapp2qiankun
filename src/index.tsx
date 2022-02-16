4cdd2ac0baa4d2eff2351bfb82dfe937bd421a8d
saranyasris <dentist.apassionateprogrammer@gmail.com>
2022-02-16 17:54:27 +0530

    added subapp changes for qiankun working

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "./config/Init_qiankun";
+import "../src/config/init-qiankun";
 ReactDOM.render(
   <React.StrictMode>
     <App />
